972770eb0a65c4db23a7e0935d0ea5f3b6c54a1a
zWilter <83887701+zWilter@users.noreply.github.com>
2024-08-09 17:28:24 -0300

    entrega de pagina principal junto a la de publicaciones

---
+++ app.js
@@ -0,0 +1,47 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+const body = document.querySelector("body");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    body.classList.add("no-scroll");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    body.classList.remove("no-scroll");
+});
+
+var swiper = new Swiper('.swiper-container', {
+	
+    navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev'
+	},
+
+	slidesPerView: 1,
+	spaceBetween: 10,
+
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+
+	breakpoints: {
+	  620: {
+		slidesPerView: 1,
+		spaceBetween: 20,
+	  },
+	  680: {
+		slidesPerView: 2,
+		spaceBetween: 40,
+	  },
+	  920: {
+		slidesPerView: 3,
+		spaceBetween: 40,
+	  },
+	} 
+});
+
+
