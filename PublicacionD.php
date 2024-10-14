10d419c8e2db3d238106657a7325a247546e024a
zWilter <83887701+zWilter@users.noreply.github.com>
2024-10-14 18:29:49 -0300

    Cambio en flechita

--- PublicacionD.php
+++ PublicacionD.php
@@ -74,7 +74,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['id_prod'])) {
         <div class="divprincipalcomentarios">
             <img class="imagenperfilcomentarios" src="<?php echo $foto?>" alt="img">
             <input type="text" class="inputcomentarios" placeholder="Comentario" aria-label="Comentario" name="comentario">
-            <input type="submit" class="botoncomentar" name="envio-com" ><i class="fa-solid fa-arrow-right"></i></input>
+            <input type="submit" class="botoncomentar" name="envio-com" ></input>
         </div>
         </form>
         <div class="divinfocomentarios">
